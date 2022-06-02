c47002bc581f0f43cb37da691007ff08af912b95
simransdsu <simran5590@gmail.com>
2022-06-01 18:16:43 -0700

    Initial Commit

---
+++ Shared/CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanionApp.swift
+//  Shared
+//
+//  Created by Simran Preet Singh Narang on 2022-06-01.
+//
+
+import SwiftUI
+
+@main
+struct CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Simran Preet Singh Narang on 2022-06-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
